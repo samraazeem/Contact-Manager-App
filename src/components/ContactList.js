758b51d35db152d3c11e97998f533039f8dd7802
samraazeem <62462084+samraazeem@users.noreply.github.com>
2021-10-02 14:31:52 +0530

    adding components

---
+++ src/components/ContactList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const ContactList = () => {
+        return (
+            <div className="ui celled list">
+                Contact List
+            </div>
+        )
+    }
+
+export default ContactList
