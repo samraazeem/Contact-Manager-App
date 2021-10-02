f59c179418dac818c2702f588cf4327058e7a6bd
spike494 <72295925+spike494@users.noreply.github.com>
2021-10-03 02:20:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-  const contact = [
-    {
-      id: "1",
-      name: "Thor",
-      email: "thor.odinson@avengers.com"
-    },
-    {
-      id: "2",
-      name: "Captain America",
-      email: "steve@avengers.com"
-    },
-    {
-      id: "1",
-      name: "Iron Man",
-      email: "tony.stark@avengers.com"
-    }
-  ]
+
+  const LOCAL_STORAGE_KEY= "contacts";
+  const [contacts, setContact] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContact([...contacts, contact]);
+  };
+
+  useEffect(() => {
+    const getContacts= JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (getContacts) setContact(getContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact />      
-      <ContactList contacts= {contact}/>
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
